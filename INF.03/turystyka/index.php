64affe92579020112ce01bc71e50dca1ec63f9e9
Szkola <jan.kowalski@gmail.com>
2026-01-23 09:46:45 +0100

    dokonczenie egzaminu turystyka

--- INF.03/turystyka/index.php
+++ INF.03/turystyka/index.php
@@ -24,13 +24,20 @@
             <aside class="boczny">
                 <h3>Twój cel wyprawy</h3>
                 <form action="" method="post">
-                    <label for="miejsce_wycieczki">Miejsce wycieczk</label>
+                    <label for="miejsce_wycieczki">Miejsce wycieczki</label> <br />
                     <select name="miejsce_wycieczki">
                         <!--Skrypt 1-->
                         <?php
-                            $sql = "SELECT nazwa FROM miejsca ORDER BY nazwa ASC";
+                            $zapytanie = "SELECT nazwa FROM miejsca ORDER BY nazwa ASC";
+                            $wynik = mysqli_query($mysqli, $zapytanie);
+
+                            while($rekord = mysqli_fetch_array($wynik)) 
+                            {
+                                echo "<option>" . $rekord['nazwa'] . "</option>";
+                            }
                         ?>
                     </select>
+                    <br />
 
                     <label for="ile_doroslych">Ile dorosłych?</label> <br /> 
                     <input type="number" name="ile_doroslych" /> <br />
@@ -41,16 +48,51 @@
                     <label for="termin">Termin</label> <br />
                     <input type="date" name="termin" /> <br />
 
-                    <button type="submit">Symulacja ceny</button>
+                    <button type="submit" name="symulacja_ceny">Symulacja ceny</button>
                 </form>
 
                 <h4>Koszt wycieczki</h4>
                 <!--Skrypt 2-->
+                <?php
+                    if (isset($_POST['symulacja_ceny'])) 
+                    {
+                        $zapytanie = "SELECT cena FROM miejsca WHERE nazwa = '" . $_POST['miejsce_wycieczki'] . "';";
+                        $wynik = mysqli_query($mysqli, $zapytanie);
+
+                        while($rekord = mysqli_fetch_array($wynik)) 
+                        {
+                            $ile_doroslych = floatval($_POST['ile_doroslych']) ?? 0;
+                            $ile_dzieci = floatval($_POST['ile_dzieci']) ?? 0;
+                            $cena = floatval($rekord['cena']);
+                            
+                            $do_zaplaty = $cena * $ile_doroslych + ($cena / 2) * $ile_dzieci;
+                            $termin = $_POST['termin'];
+
+                            echo "<p>W dniu: " . $termin . "</p>";
+                            echo "<p>" . $do_zaplaty . " złotych</p>";
+                        }
+                    }
+                ?>
             </aside>
 
             <section class="sekcja">
                 <h3>Wycieczki</h3>
                 <!--Skrypt 3-->
+                <?php
+                    $zapytanie = "SELECT nazwa, cena, link_obraz FROM miejsca WHERE link_obraz LIKE '0%'";
+                    $wynik = mysqli_query($mysqli, $zapytanie);
+
+                    while($rekord = mysqli_fetch_array($wynik)) 
+                    {
+                        echo '<div class="wycieczka">';
+                        echo '<img src="'. $rekord['link_obraz'] . '" alt="zdjęcie z wycieczki" />';
+                        echo '<h2>' . $rekord['nazwa'] . '</h2>';
+                        echo '<p>' . $rekord['cena'] . '</p>';
+                        echo '</div>';
+                    }
+
+                    
+                ?>
             </section>
         </main>
 
